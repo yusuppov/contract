1feef6f84136d47835d8a6ed00863ab302734662
Ramil <Ramil>
2026-02-03 15:17:22 +0300

    widgets/CollapsBlock

--- src/Pages/HomePage/ui/HomePage.tsx
+++ src/Pages/HomePage/ui/HomePage.tsx
@@ -4,6 +4,8 @@ import { ContainerFlex } from "../../../widgets/ContainerFlex";
 import { Text } from "../../../shared/Components/Text";
 import { Card } from "../../../shared/Components/Card/ui";
 import { InfoCard } from "../../../widgets/InfoCard";
+import { CollapsBlock } from "../../../widgets/CollapsBlock";
+
 import Icon from "../../../shared/assets/icons/personIcon.svg";
 
 export const HomePage = () => {
@@ -24,6 +26,13 @@ export const HomePage = () => {
           circleColor="green"
           icon={<Icon />}
         ></InfoCard>
+        <CollapsBlock title="В чем разница между контрактниками и добровольцами">
+          Контрактники являются военнослужащими. В контракте о прохождении
+          военной службы указывается срок, в течение которого гражданин
+          обязуется проходить военную службу, условия контракта и права
+          военнослужащего Участники добровольческих формирований не являются
+          военнослужащими
+        </CollapsBlock>
       </div>
       <div className={styles.main}></div>
     </div>
---
+++ src/widgets/CollapsBlock/ui/CollapsBlock.tsx
@@ -0,0 +1,28 @@
+import '../../../feature/utils.css'
+import { FC, useState } from "react";
+
+import { Text } from "../../../shared/Components/Text";
+
+import styles from "./CollapsBlock.module.css";
+import clsx from "clsx";
+
+interface CollapsBlockInterface {
+  title: string;
+  children: string;
+}
+
+export const CollapsBlock: FC<CollapsBlockInterface> = ({
+  title,
+  children,
+}) => {
+  const [active, setActive] = useState(false);
+  const activeFunc = () => setActive(!active);
+  return (
+    <div onClick={activeFunc} className={clsx(styles.collapsBlock)}>
+      <div className={clsx(styles.collapsBlockTitle)}>
+        <Text variant="body1">{title}</Text> <span className={active ? 'rotate' : ''}>{">"}</span>
+      </div>
+      {active ? <Text variant="h4">{children}</Text> : null}
+    </div>
+  );
+};
