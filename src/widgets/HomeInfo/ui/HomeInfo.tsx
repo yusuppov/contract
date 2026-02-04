0a9652941a24d174c958ae414cc79197148a18c0
Ramil <Ramil>
2026-02-04 17:08:53 +0300

    widgets/change

--- src/widgets/HomeInfo/ui/HomeInfo.tsx
+++ src/widgets/HomeInfo/ui/HomeInfo.tsx
@@ -1,68 +1,51 @@
 import { ContainerFlex } from "../../ContainerFlex";
 import { Text } from "../../../shared/Components/Text";
 import { InfoCard } from "../../InfoCard";
-import Icon from "../../../shared/assets/img/document.svg";
-import IconMan from "../../../shared/assets/icons/personIcon.svg";
+
 import styles from "./HomeInfo.module.css";
-import clsx from "clsx";
 import "../../../feature/utils.css";
+
+import Icon from "../../../shared/assets/img/document.svg";
+import IconFile1 from "../../../shared/assets/icons/file-contract-solid.svg";
+import IconFile2 from "../../../shared/assets/icons/file-lines-solid.svg";
+import IconFile3 from "../../../shared/assets/icons/handshake-solid.svg";
+import IconFile4 from "../../../shared/assets/icons/stethoscope-solid.svg";
 
 export const HomeInfo = () => {
   return (
-    <div className="mb72">
+    <div>
       <ContainerFlex>
         <Text className={styles.containerTopTitle} variant="h2">
-          Как стать военнослужащим по контракту
+          Этапы подписания контракта
         </Text>
         <div className={styles.containerTopIcon}>
           <Icon></Icon>
         </div>
       </ContainerFlex>
-      <div className={styles.cardsGrid}>
-        <InfoCard
-          icon={<IconMan />}
-          title="Гражданину РФ"
-          className={styles.cardGrid1}
-          btnText="Подать заявление"
-          mainText="На Госуслугах, в пункте отбора или военном комиссариате"
-        ></InfoCard>
-        <InfoCard
-          icon={<IconMan />}
-          title="Иностранцу"
-          className={styles.cardGrid2}
-          btnText="Найти пункт отбора"
-          mainText="В пункте отбора или военном комиссариате"
-        ></InfoCard>
-        <InfoCard className={styles.cardGrid3} mainText="">
-          <ol>
-            <li className={clsx(styles.cardListLi, "mb16")}>
-              <Text variant="h4">
-                Подготовьте документы:
-                <ul className={styles.listUl}>
-                  <li>паспорт</li>
-                  <li>военный билет — при наличии</li>
-                  <li>свидетельство о браке и рождении детей — при наличии</li>
-                  <li>анкету — можно заполнить заранее или в пункте отбора</li>
-                </ul>
-              </Text>
-            </li>
-            <li className={clsx(styles.cardListLi, "mb16")}>
-              <Text variant="h4">
-                Пройдите собеседование с психологом в пункте отбора или военном
-                комиссариате
-              </Text>
-            </li>
-            <li className={clsx(styles.cardListLi, "mb16")}>
-              <Text variant="h4">
-                Пройдите медицинский осмотр в военном комиссариате
-              </Text>
-            </li>
-            <li className={styles.cardListLi}>
-              Заключите контракт в пункте отбора или в воинской части
-            </li>
-          </ol>
-        </InfoCard>
-      </div>
+      <ContainerFlex>
+        <div className={styles.cardsGrid}>
+          <InfoCard
+            className="blueBorder"
+            title="Оставить заявку"
+            icon={<IconFile1 />}
+          ></InfoCard>
+          <InfoCard
+            className="blueBorder"
+            title="Пройти собеседование"
+            icon={<IconFile2 />}
+          ></InfoCard>
+          <InfoCard
+            className="blueBorder"
+            title="Мед. комисиия"
+            icon={<IconFile3 />}
+          ></InfoCard>
+          <InfoCard
+            className="blueBorder"
+            title="Заключить контракт"
+            icon={<IconFile4 />}
+          ></InfoCard>
+        </div>
+      </ContainerFlex>
     </div>
   );
 };
