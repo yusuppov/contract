e8eccca25ab42e9271d4c388d3ee68227bd5d713
Ramil <Ramil>
2026-02-05 21:52:43 +0300

    main/fixRouting

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ModalProvider } from "./shared/Providers/ModalProvider";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import "normalize.css";
 import "./index.css";
 import App from "./App/App";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <StrictMode>
       <ModalProvider>
         <App />
       </ModalProvider>
     </StrictMode>
-  </BrowserRouter>,
+  </HashRouter>,
 );
