b6343aecf6ab4cd09b0861d160c46173b6c6cd6c
Ramil <Ramil>
2026-02-05 20:21:53 +0300

    widget/fixForm-addPolicePages

--- src/shared/Providers/ModalProvider/ModalProvider.tsx
+++ src/shared/Providers/ModalProvider/ModalProvider.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { ModalContext } from "../ModalContext";
-import styles from "./ModalProvider.module.css";
 
 import { Form } from "../../../widgets/Form";
 
@@ -10,12 +9,11 @@ export const ModalProvider = ({ children }: { children: React.ReactNode }) => {
   const close = () => setIsOpen(false);
   useEffect(() => {
     if (isOpen) {
-      document.body.style.overflow = "hidden"; // блокируем
+      document.body.style.overflow = "hidden";
     } else {
-      document.body.style.overflow = ""; // возвращаем
+      document.body.style.overflow = "";
     }
 
-    // очистка при размонтировании
     return () => {
       document.body.style.overflow = "";
     };
@@ -38,41 +36,13 @@ export const ModalProvider = ({ children }: { children: React.ReactNode }) => {
           onClick={close}
         >
           <div
-            style={{ background: "white", padding: 30, borderRadius: 12 }}
+            style={{ background: "white", padding: 50, borderRadius: 12 }}
             onClick={(e) => e.stopPropagation()}
           >
             <Form />
-            <button
-              className={styles.formButton}
-              onClick={close}
-              style={{ marginTop: 16 }}
-            >
-              Закрыть
-            </button>
           </div>
         </div>
       )}
     </ModalContext.Provider>
   );
 };
-
-// <div
-//           style={{
-//             position: "fixed",
-//             inset: 0,
-//             background: "rgba(0,0,0,0.5)",
-//             display: "flex",
-//             justifyContent: "center",
-//             alignItems: "center",
-//             zIndex: 9999,
-//           }}
-//           onClick={close}
-//         >
-//           <div
-//             style={{ background: "white", padding: 30, borderRadius: 12 }}
-//             onClick={(e) => e.stopPropagation()}
-//           >
-//             <h2>Форма</h2>
-//             <button onClick={close}>Закрыть</button>
-//           </div>
-//         </div>
