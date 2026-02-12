542d9760341f7128009c11e0bcef4e82c5065b30
Ramil <Ramil>
2026-02-12 14:07:18 +0300

    widgets/fixRewardDelPages

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,17 +1,8 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
 import { HomePage } from "../Pages/HomePage";
-import { ConfPage } from "../Pages/ConfPage";
-import { PersonalPage } from "../Pages/PersonalPage";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/conf" element={<ConfPage />} />
-      <Route path="/personal" element={<PersonalPage />}></Route>
-    </Routes>
-  );
+  return <HomePage />;
 }
 
 export default App;
